ffcf6c7a79fbe1c9e5d8781d5229e39aa650f06b
Mauricio <may.power@hotmail.com>
2020-03-24 18:19:25 -0300

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 
 require("./config/config")
 
-const express = require('express')
+const express = require('express');
+const mongoose = require("mongoose");
+
 const app = express()
+
+
 
 const bodyParser = require("body-parser");
 
@@ -11,47 +15,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
  
+
+app.use(require("./routes/usuario"));
  
-app.get('/usuario', function (req, res) {
-  res.json('Get usuario')
-})
 
-app.post('/usuario', function (req, res) {
-
-    let body = req.body;
-
-    if(body.nombre === undefined){
-
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        })
-
-    }else{
-
-        res.json({
-            persona: body
-        })
-    }
-
-
-    
-})
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-
-})
-
-app.delete('/usuario', function (req, res) {
-    res.json('Delete usuario')
-})
+mongoose.connect(process.env.urlDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}, () => {
+    console.log("Base de datos ONLINE");
+});
  
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando el puerto 3000`);
 });
+
